bb8de2abef79390e7b24d90ff0eb312ed9241157
ionut <ionutcatana1805@gmail.com>
2023-12-04 05:04:16 +0000

    Add project files.

---
+++ Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    internal class Method
+    {
+        // Creating a method with two integers as parameters
+        // Making one integer optional by giving it a default value
+        public int MultiOp(int a, int b = 18) 
+        {
+            // Multiplying the integers between each other and returning the result
+            int result = a * b;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instatiating Method class
+            Method mathNumber = new Method();
+            // Asking the user to input first number
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            // Asking the user to input second number or skip by pressing enter
+            Console.WriteLine("Please enter a second number, or press enter to skip this step: ");
+
+            try
+            {
+                // Calling method with 2 numbers if both numbers are provided by the user
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int results = mathNumber.MultiOp(num1, num2);
+                Console.WriteLine(num1 + " multiply by " + num2 + " equals to " + results);
+            }
+            catch 
+            {
+                // Calling method with only one number provided by the user
+                int results = mathNumber.MultiOp(num1);
+                Console.WriteLine(num1 + " multiply by our default number equals to " + results);
+            }
+            Console.ReadLine();
+        }
+    }
+}
